57bdbbff6994099bde921bd9b9a188294c0735b6
ShADAMoV <adam.shad@mail.ru>
2022-12-20 14:59:13 +0300

    header add

--- beautySalon/dev/src/scripts/main.js
+++ beautySalon/dev/src/scripts/main.js
@@ -1,2 +1,8 @@
-import 'normalize.css';
 import '../styles/main.scss';
+
+window.addEventListener("load", function () {
+    this.document.querySelector(".header-mobile__content").addEventListener("click", function () {
+        this.classList.toggle("header-mobile_active");
+        document.querySelector(".burger-menu").classList.toggle('burger-menu_active');
+    })
+})
