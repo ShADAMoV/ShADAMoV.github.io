c9f9b057aaaf8fc18bd95ead4ff7014fb9ce2c35
ShADAMoV <adam.shad@mail.ru>
2023-01-18 15:05:51 +0300

    уничтожил ничтожество

--- turboru/dev/src/scripts/components/header.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", function () {
-    document.querySelector(".header__burger").addEventListener('click', function () {
-        this.classList.toggle("header__burger_active");
-        document.querySelector(".menu").classList.toggle('show');
-    });
-});
